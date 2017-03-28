f70f807bd861ec7fba6ebecc162b7f909a310bec
Derek Rife <derek.r6530@gmail.com>
2017-03-27 19:53:39 -0500

    fixed welcome redirect (just needed username query)

--- main.py
+++ main.py
@@ -17,33 +17,33 @@ page_header = """
 """
 
 form = """
-        <form action="/welcome" method="post">
+        <form method="post">
             <table>
                 <tr>
                     <td><label for="username">Username</label></td>
                     <td>
-                        <input name="username" type="text" value="" required>
+                        <input name="username" type="text" value="%(username)s" required/>
                         <span class="error">%(username_error)s</span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="password">Password</label></td>
                     <td>
-                        <input name="password" type="password" required>
+                        <input name="password" type="password" required/>
                         <span class="error">%(password_error)s</span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="verify">Verify Password</label></td>
                     <td>
-                        <input name="verify" type="password" required>
+                        <input name="verify" type="password" required/>
                         <span class="error">%(verify_error)s</span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="email">Email (optional)</label></td>
                     <td>
-                        <input name="email" type="email" value="">
+                        <input name="email" type="email" value="%(email)s"/>
                         <span class="error">%(email_error)s</span>
                     </td>
                 </tr>
@@ -95,17 +95,43 @@ class MainHandler(webapp2.RequestHandler):
         self.write_form()
 
     def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
 
+        error = False
 
+        username_error = ""
+        if not valid_username(username):
+            username_error = "Not a valid username."
+            error = True
 
-        self.write_form(username,email,username_error,password_error,
-                        verify_error,email_error)
+        password_error = ""
+        if not valid_password(password):
+            password_error = "Not a valid password."
+            error = True
+
+        verify_error = ""
+        if not password==verify:
+            verify_error = "Passwords do not match."
+            error = True
+
+        email_error = ""
+        if email and not valid_email(email):
+            email_error = "Not a valid email."
+
+        if error:
+            self.write_form(username, password, verify, email,
+                username_error, password_error, verify_error, email_error)
+        else:
+            self.redirect("/welcome?username="+username)
 
 class WelcomeHandler(webapp2.RequestHandler):
-    def post(self):
+    def get(self):
         username = self.request.get("username")
         welcome = "<h1>Welcome, " + username + "!</h1>"
-        self.response.write(page_header + welcome + page_footer)
+        self.response.out.write(welcome)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
